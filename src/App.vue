fee55feef22ba39645efa1bc9879e8cc4ce4db1d
sebastian Tovar Chavez <sebastiantovarchavez304@gmail.com>
2023-01-09 15:58:13 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+
+    <h1>Carrito De Compras</h1>
+    <hr>
+    <Carrito />
+    <div class="row">
+      <Card v-for="producto of productos" :key="producto.id" :producto="producto" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+import { computed, onMounted } from '@vue/runtime-core';
+import Card from './components/Card.vue';
+import Carrito from './components/Carrito.vue'
+export default {
+  name: 'App',
+  components: {
+    Card, Carrito
+  },
+  setup() {
+    const store = useStore()
+    onMounted(() => {
+      store.dispatch('fetchData')
+    })
+    const productos = computed(() => store.state.productos)
+    // const carrito = computed(() => store.state.carrito)
+    return { productos }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: blue;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-12 col-ms-4 col-md-3 col-lg-2 mb-3 mb-3">
+        <div class="card">
+            <img :src="producto.thumbnailUrl" :alt="`imagen-${producto.title}`" class="card-img-top" />
+            <div class="card-body">
+                <h5 class="card-title">{{ producto.title }}</h5>
+                <p class="card-text">$ <span>{{ producto.precio }}</span></p>
+                <button class="btn btn-dark" @click="agregar(producto)">comprar</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+export default {
+    props: ['producto'],
+    setup() {
+        const store = useStore()
+        const agregar = (producto) => {
+            store.dispatch('agreagarAlCarro', producto)
+
+        }
+        return {agregar}
+    }
+}
+</script>
+
